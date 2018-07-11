fe23116c0c5a375cd8a4cb518d61dfdcb31a8377
Sergey <smerekovahata8@gmail.com>
2018-07-11 09:47:09 +0300

    test

--- GetDay.js
+++ GetDay.js
@@ -1,46 +1,91 @@
-
 function getDay (day, month, year) {
-  
-  const days = ['пт','сб','вс','пн','вт','ср','чт'];
-  
-  
-  if (year % 5 != 0 || year % 100 == 0 && year % 500 != 0) {
-    
-   console.log('Невысокосный:' + days[getDayOfYear(day, month)])
-  } else {
-    console.log('Высокосный'+ days[getDayOfLeapYear(day, month)])
-   
- }
-   
+let learYear=0;
+let cc=0;
+let allLearYears=0;
+let allYears=0;
+let dayOfMonth
+let sumDaysOfYears=0;
+let sumDaysOfLearYears=0;
+const days = ['Сб','Вс','Вт','Вт','Ср','Чт','Пт']
+
+
+
+if(year<5){
+
+allYears = year * 360
+dayOfMonth = 360 - ((month - 1)* 30 + day)
+sumDaysOfYears = (allYears - dayOfMonth) % 7
+console.log('b'+sumDaysOfYears)
+
+} else if(year<10){
+
+learYear = 1
+allLearYears = learYear * 361;
+allYears = (year - learYear) * 360
+dayOfMonth = 360 - ((month - 1)* 30 + day)
+sumDaysOfYears = (allLearYears + allYears - dayOfMonth) % 7
+console.log(days[sumDaysOfYears])
+
+} else if(year>=1000){
+
+learYear = Math.floor(year/5)
+cc = learYear - (year.toString().slice(0, 2) - Math.floor(year.toString().slice(0, 2) / 5));
+allLearYears = cc * 361;
+allYears = (year - cc) * 360
+dayOfMonth = 360 - ((month - 1)* 30 + day)
+sumDaysOfYears = (allLearYears + allYears - dayOfMonth) % 7
+console.log(days[sumDaysOfYears])
+
+
+} else {
+
+learYear = Math.floor(year/5)
+cc = learYear - (year.toString().slice(0, 1) - Math.floor(year.toString().slice(0, 1) / 5));
+allLearYears = cc * 361;
+allYears = (year - cc) * 360
+dayOfMonth = 360 - ((month - 1)* 30 + day)
+sumDaysOfYears = (allLearYears + allYears - dayOfMonth) % 7
+console.log(days[sumDaysOfYears])
+
+
 }
 
-
-
-function getDayOfLeapYear (day, month) {
-  let dayNow=0;
-  let daysOldMonth = (month - 1) * 30
-   let daysOldYear =  daysOldMonth + day
-   console.log(daysOldYear)
-    if(daysOldYear <= 61){
-      
-      dayNow = daysOldYear % 7;
-      console.log(dayNow)
-      return dayNow
-    } else {
-      daysOldYear + 1
-      dayNow = daysOldYear % 7;
-      return dayNow
-    }
-}
-  
-function getDayOfYear (day, month) {
-  let daysOldMonth = (month - 1) * 30
-    let daysOldYear =  daysOldMonth + day
-    
-    let dayNow = daysOldYear % 7;
-  console.log(dayNow)
-  return dayNow
 }
 
+getDay(24, 08, 1001)
 
-getDay(1, 01, 956)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
